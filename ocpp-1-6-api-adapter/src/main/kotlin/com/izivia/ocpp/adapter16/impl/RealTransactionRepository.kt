f642ed29d868daef21aa6d51388c6aeda6b3e798
Lilian Gallon <lilian.gallon@4sh.fr>
2022-10-28 10:10:27 +0200

    IDEV-496: getLocalIdByTransactionId does not throw an exception if the id is not known but rather the passed transactionId

--- ocpp-1-6-api-adapter/src/main/kotlin/com/izivia/ocpp/adapter16/impl/RealTransactionRepository.kt
+++ ocpp-1-6-api-adapter/src/main/kotlin/com/izivia/ocpp/adapter16/impl/RealTransactionRepository.kt
@@ -21,10 +21,10 @@ class RealTransactionRepository : TransactionRepository {
 
     override fun getLocalIdByTransactionId(transactionId: Int): Ocpp16TransactionIds {
         val localId = hashMap.toList().find { it.second == transactionId }?.first
-        if (localId != null) {
-            return Ocpp16TransactionIds(localId, transactionId)
+        return if (localId != null) {
+            Ocpp16TransactionIds(localId, transactionId)
         } else {
-            throw IllegalStateException("value $transactionId not found in hashmap")
+            Ocpp16TransactionIds(transactionId.toString(), transactionId)
         }
     }
 }
