eb22d8fee3ef1254b6b61eb6ed5c1d5f2edd9daa
vincent.pichon <vincent.pichon@izivia.com>
2023-02-24 16:49:03 +0100

    IDEV-1018 remove 1.6 security from SOAP

--- ocpp-1-6-soap/src/main/kotlin/com/izivia/ocpp/soap16/Ocpp16SoapBody.kt
+++ ocpp-1-6-soap/src/main/kotlin/com/izivia/ocpp/soap16/Ocpp16SoapBody.kt
@@ -1,16 +1,12 @@
 package com.izivia.ocpp.soap16
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonIgnoreType
 import com.izivia.ocpp.core16.model.authorize.AuthorizeReq
 import com.izivia.ocpp.core16.model.authorize.AuthorizeResp
 import com.izivia.ocpp.core16.model.bootnotification.BootNotificationReq
 import com.izivia.ocpp.core16.model.bootnotification.BootNotificationResp
 import com.izivia.ocpp.core16.model.cancelreservation.CancelReservationReq
 import com.izivia.ocpp.core16.model.cancelreservation.CancelReservationResp
-import com.izivia.ocpp.core16.model.certificatesigned.CertificateSignedReq
-import com.izivia.ocpp.core16.model.certificatesigned.CertificateSignedResp
-import com.izivia.ocpp.core16.model.certificatesigned.DeleteCertificateResp
 import com.izivia.ocpp.core16.model.changeavailability.ChangeAvailabilityReq
 import com.izivia.ocpp.core16.model.changeavailability.ChangeAvailabilityResp
 import com.izivia.ocpp.core16.model.changeconfiguration.ChangeConfigurationReq
@@ -21,30 +17,22 @@ import com.izivia.ocpp.core16.model.clearchargingprofile.ClearChargingProfileReq
 import com.izivia.ocpp.core16.model.clearchargingprofile.ClearChargingProfileResp
 import com.izivia.ocpp.core16.model.datatransfer.DataTransferReq
 import com.izivia.ocpp.core16.model.datatransfer.DataTransferResp
-import com.izivia.ocpp.core16.model.deletecertificate.DeleteCertificateReq
 import com.izivia.ocpp.core16.model.diagnosticsstatusnotification.DiagnosticsStatusNotificationReq
 import com.izivia.ocpp.core16.model.diagnosticsstatusnotification.DiagnosticsStatusNotificationResp
 import com.izivia.ocpp.core16.model.firmwarestatusnotification.FirmwareStatusNotificationReq
 import com.izivia.ocpp.core16.model.firmwarestatusnotification.FirmwareStatusNotificationResp
-import com.izivia.ocpp.core16.model.firmwarestatusnotification.SignedFirmwareStatusNotificationReq
-import com.izivia.ocpp.core16.model.firmwarestatusnotification.SignedFirmwareStatusNotificationResp
 import com.izivia.ocpp.core16.model.getcompositeschedule.GetCompositeScheduleReq
 import com.izivia.ocpp.core16.model.getcompositeschedule.GetCompositeScheduleResp
 import com.izivia.ocpp.core16.model.getconfiguration.GetConfigurationReq
 import com.izivia.ocpp.core16.model.getconfiguration.GetConfigurationResp
 import com.izivia.ocpp.core16.model.getdiagnostics.GetDiagnosticsReq
 import com.izivia.ocpp.core16.model.getdiagnostics.GetDiagnosticsResp
-import com.izivia.ocpp.core16.model.getinstalledcertificateids.GetInstalledCertificateIdsReq
-import com.izivia.ocpp.core16.model.getinstalledcertificateids.GetInstalledCertificateIdsResp
 import com.izivia.ocpp.core16.model.getlocallistversion.GetLocalListVersionReq
 import com.izivia.ocpp.core16.model.getlocallistversion.GetLocalListVersionResp
 import com.izivia.ocpp.core16.model.getlog.GetLogReq
 import com.izivia.ocpp.core16.model.getlog.GetLogResp
 import com.izivia.ocpp.core16.model.heartbeat.HeartbeatReq
 import com.izivia.ocpp.core16.model.heartbeat.HeartbeatResp
-import com.izivia.ocpp.core16.model.installcertificate.InstallCertificateReq
-import com.izivia.ocpp.core16.model.logstatusnotification.LogStatusNotificationReq
-import com.izivia.ocpp.core16.model.logstatusnotification.LogStatusNotificationResp
 import com.izivia.ocpp.core16.model.metervalues.MeterValuesReq
 import com.izivia.ocpp.core16.model.metervalues.MeterValuesResp
 import com.izivia.ocpp.core16.model.remotestart.RemoteStartTransactionReq
@@ -55,28 +43,21 @@ import com.izivia.ocpp.core16.model.reservenow.ReserveNowReq
 import com.izivia.ocpp.core16.model.reservenow.ReserveNowResp
 import com.izivia.ocpp.core16.model.reset.ResetReq
 import com.izivia.ocpp.core16.model.reset.ResetResp
-import com.izivia.ocpp.core16.model.securityeventnotification.SecurityEventNotificationReq
-import com.izivia.ocpp.core16.model.securityeventnotification.SecurityEventNotificationResp
 import com.izivia.ocpp.core16.model.sendlocallist.SendLocalListReq
 import com.izivia.ocpp.core16.model.sendlocallist.SendLocalListResp
 import com.izivia.ocpp.core16.model.setchargingprofile.SetChargingProfileReq
 import com.izivia.ocpp.core16.model.setchargingprofile.SetChargingProfileResp
-import com.izivia.ocpp.core16.model.signcertificate.SignCertificateReq
-import com.izivia.ocpp.core16.model.signcertificate.SignCertificateResp
 import com.izivia.ocpp.core16.model.starttransaction.StartTransactionReq
 import com.izivia.ocpp.core16.model.starttransaction.StartTransactionResp
 import com.izivia.ocpp.core16.model.statusnotification.StatusNotificationReq
 import com.izivia.ocpp.core16.model.statusnotification.StatusNotificationResp
 import com.izivia.ocpp.core16.model.stoptransaction.StopTransactionReq
 import com.izivia.ocpp.core16.model.stoptransaction.StopTransactionResp
-import com.izivia.ocpp.core16.model.triggermessage.ExtendedTriggerMessageReq
-import com.izivia.ocpp.core16.model.triggermessage.ExtendedTriggerMessageResp
 import com.izivia.ocpp.core16.model.triggermessage.TriggerMessageReq
 import com.izivia.ocpp.core16.model.triggermessage.TriggerMessageResp
 import com.izivia.ocpp.core16.model.unlockconnector.UnlockConnectorReq
 import com.izivia.ocpp.core16.model.unlockconnector.UnlockConnectorResp
 import com.izivia.ocpp.core16.model.updatefirmware.SignedUpdateFirmwareReq
-import com.izivia.ocpp.core16.model.updatefirmware.UpdateFirmwareReq
 import com.izivia.ocpp.core16.model.updatefirmware.UpdateFirmwareResp
 import com.izivia.ocpp.soap.SoapBody
 import com.izivia.ocpp.soap.SoapFault
@@ -89,8 +70,6 @@ data class Ocpp16SoapBody(
     val bootNotificationResponse: BootNotificationResp? = null,
     val cancelReservationRequest: CancelReservationReq? = null,
     val cancelReservationResponse: CancelReservationResp? = null,
-    val certificateSignedRequest: CertificateSignedReq? = null,
-    val certificateSignedResponse: CertificateSignedResp? = null,
     val changeAvailabilityRequest: ChangeAvailabilityReq? = null,
     val changeAvailabilityResponse: ChangeAvailabilityResp? = null,
     val changeConfigurationRequest: ChangeConfigurationReq? = null,
@@ -101,12 +80,8 @@ data class Ocpp16SoapBody(
     val clearChargingProfileResponse: ClearChargingProfileResp? = null,
     val dataTransferRequest: DataTransferReq? = null,
     val dataTransferResponse: DataTransferResp? = null,
-    val deleteCertificateRequest: DeleteCertificateReq? = null,
-    val deleteCertificateResponse: DeleteCertificateResp? = null,
     val diagnosticsStatusNotificationRequest: DiagnosticsStatusNotificationReq? = null,
     val diagnosticsStatusNotificationResponse: DiagnosticsStatusNotificationResp? = null,
-    val extendedTriggerMessageRequest: ExtendedTriggerMessageReq? = null,
-    val extendedTriggerMessageResponse: ExtendedTriggerMessageResp? = null,
     val firmwareStatusNotificationRequest: FirmwareStatusNotificationReq? = null,
     val firmwareStatusNotificationResponse: FirmwareStatusNotificationResp? = null,
     val getCompositeScheduleRequest: GetCompositeScheduleReq? = null,
@@ -115,18 +90,12 @@ data class Ocpp16SoapBody(
     val getConfigurationResponse: GetConfigurationResp? = null,
     val getDiagnosticsRequest: GetDiagnosticsReq? = null,
     val getDiagnosticsResponse: GetDiagnosticsResp? = null,
-    val getInstalledCertificateIdsRequest: GetInstalledCertificateIdsReq? = null,
-    val getInstalledCertificateIdsResponse: GetInstalledCertificateIdsResp? = null,
     val getLocalListVersionResponse: GetLocalListVersionResp? = null,
     val getLocalListVersionRequest: GetLocalListVersionReq? = null,
     val getLogRequest: GetLogReq? = null,
     val getLogResponse: GetLogResp? = null,
     val heartbeatRequest: HeartbeatReq? = null,
     val heartbeatResponse: HeartbeatResp? = null,
-    val installCertificateRequest: InstallCertificateReq? = null,
-    val installedCertificateIdsResponse: GetInstalledCertificateIdsResp? = null,
-    val logStatusNotificationRequest: LogStatusNotificationReq? = null,
-    val logStatusNotificationResponse: LogStatusNotificationResp? = null,
     val meterValuesRequest: MeterValuesReq? = null,
     val meterValuesResponse: MeterValuesResp? = null,
     val remoteStartTransactionRequest: RemoteStartTransactionReq? = null,
@@ -137,18 +106,10 @@ data class Ocpp16SoapBody(
     val reserveNowResponse: ReserveNowResp? = null,
     val resetRequest: ResetReq? = null,
     val resetResponse: ResetResp? = null,
-    val securityEventNotificationRequest: SecurityEventNotificationReq? = null,
-    val securityEventNotificationResponse: SecurityEventNotificationResp? = null,
     val sendLocalListRequest: SendLocalListReq? = null,
     val sendLocalListResponse: SendLocalListResp? = null,
     val setChargingProfileRequest: SetChargingProfileReq? = null,
     val setChargingProfileResponse: SetChargingProfileResp? = null,
-    val signCertificateRequest: SignCertificateReq? = null,
-    val signCertificateResponse: SignCertificateResp? = null,
-    val signedFirmwareStatusNotificationRequest: SignedFirmwareStatusNotificationReq? = null,
-    val signedFirmwareStatusNotificationResponse: SignedFirmwareStatusNotificationResp? = null,
-    val signedUpdateFirmwareRequest: SignedUpdateFirmwareReq? = null,
-    val signedUpdateFirmwareResponse: UpdateFirmwareResp? = null,
     val startTransactionRequest: StartTransactionReq? = null,
     val startTransactionResponse: StartTransactionResp? = null,
     val statusNotificationRequest: StatusNotificationReq? = null,
