fc28bf5995798b49e04545c108cc575b50d70f17
Deborah Pereira <deborah.pereira@4sh.fr>
2023-03-29 14:17:53 +0200

    feat: IDEV-1643 add mixin to not parse timestamp field

--- ocpp-2-0-json/src/main/kotlin/com/izivia/ocpp/json20/Ocpp20JsonObjectMapper.kt
+++ ocpp-2-0-json/src/main/kotlin/com/izivia/ocpp/json20/Ocpp20JsonObjectMapper.kt
@@ -1,9 +1,14 @@
 package com.izivia.ocpp.json20
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonValue
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.izivia.ocpp.core20.model.common.enumeration.MeasurandEnumType
 import com.izivia.ocpp.core20.model.common.enumeration.ReadingContextEnumType
+import com.izivia.ocpp.core20.model.notifycustomerinformation.NotifyCustomerInformationReq
+import com.izivia.ocpp.core20.model.notifyevent.NotifyEventReq
+import com.izivia.ocpp.core20.model.notifymonitoringreport.NotifyMonitoringReportReq
+import com.izivia.ocpp.core20.model.notifyreport.NotifyReportReq
 import com.izivia.ocpp.core20.model.notifyreport.enumeration.DataEnumType
 import com.izivia.ocpp.json.OcppJsonMapper
 
@@ -12,8 +17,15 @@ internal object Ocpp20JsonObjectMapper : ObjectMapper(
         .addMixIn(MeasurandEnumType::class.java, EnumMixin::class.java)
         .addMixIn(ReadingContextEnumType::class.java, EnumMixin::class.java)
         .addMixIn(DataEnumType::class.java, EnumMixin::class.java)
+        .addMixIn(NotifyCustomerInformationReq::class.java, HasActionTimestampMixin::class.java)
+        .addMixIn(NotifyEventReq::class.java, HasActionTimestampMixin::class.java)
+        .addMixIn(NotifyReportReq::class.java, HasActionTimestampMixin::class.java)
+        .addMixIn(NotifyMonitoringReportReq::class.java, HasActionTimestampMixin::class.java)
 )
 
 private abstract class EnumMixin(
     @JsonValue val value: String
 )
+
+@JsonIgnoreProperties(value = ["timestamp"])
+private abstract class HasActionTimestampMixin
--- utils/src/main/kotlin/com/izivia/ocpp/utils/HasActionTimestamp.kt
+++ utils/src/main/kotlin/com/izivia/ocpp/utils/HasActionTimestamp.kt
@@ -2,6 +2,10 @@ package com.izivia.ocpp.utils
 
 import kotlinx.datetime.Instant
 
+/**
+ * Interface implemented by OCPP request containing a timestamp field in its payload.
+ * @property timestamp : the date and time at which this action occurred
+ */
 interface HasActionTimestamp {
 
     val timestamp: Instant?
